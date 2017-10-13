a5e51fc49da74ce68e4b88bf1c4464c88aad556e
GeorgeSalari <x3m.giku@gmail.com>
2017-10-13 17:18:06 +0800

    add role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013063626) do
+ActiveRecord::Schema.define(version: 20171013091734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171013063626) do
     t.string "provider"
     t.string "uid"
     t.string "oauth_token"
+    t.integer "role"
   end
 
 end
