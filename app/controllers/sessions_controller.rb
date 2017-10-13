a53c2f669c020dc0ea861110cb8c01d653d62653
GeorgeSalari <x3m.giku@gmail.com>
2017-10-13 11:53:17 +0800

    1. Add gem bcrypt for sequre password 2. Generate User model 3. Generate Users controller 4. In root file create path for user (create, edit, show, update) 5. Generate Welcome controller with index method 6. Crete index.html.erb for welcom controller 7. Generate Session model 8. Generate Sessions controller 9. In root file create path only for create and destroy sessions 10. Add helpers for user to find current_user and to check if the user is logged_in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:notice] = "Hello #{@user.first_name}"
+        redirect_to user_path(@user)
+      else
+        @user = User.new
+        flash.now[:error] = "Incorect password!"
+        render "welcome/index"
+      end
+    else
+      @user = User.new
+      flash[:error] = "No such email in our database!"
+      render "welcome/index"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+  resources :users, except: [:index, :destroy, :new]
+  put 'sign_in' => 'sessions#create', as: :sign_in
+  delete 'log_out' => 'sessions#destroy', as: :log_out
 end
