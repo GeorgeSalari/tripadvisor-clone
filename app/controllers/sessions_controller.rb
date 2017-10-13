60ea533e007bc8d605c9248f97f9b7ff236667c2
GeorgeSalari <x3m.giku@gmail.com>
2017-10-13 17:05:34 +0800

    fix normal sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,27 @@
 class SessionsController < ApplicationController
   def create
-    @user_omn = User.from_omniauth(request.env["omniauth.auth"])
-    if @user_omn
-      session[:user_id] = @user_omn.id
-      redirect_to user_path(@user_omn)
-    else
-      @user = User.find_by(email: params[:email])
-      if @user
-        if @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          flash[:notice] = "Hello #{@user.first_name}"
-          redirect_to user_path(@user)
-        else
-          @user = User.new
-          flash.now[:error] = "Incorect password!"
-          render "welcome/index"
-        end
+    @user = User.find_by(email: params[:email])
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:notice] = "Hello #{@user.first_name}"
+        redirect_to user_path(@user)
       else
         @user = User.new
-        flash[:error] = "No such email in our database!"
+        flash.now[:error] = "Incorect password!"
         render "welcome/index"
       end
+    else
+      @user = User.new
+      flash[:error] = "No such email in our database!"
+      render "welcome/index"
     end
+  end
+
+  def create_google
+    @user_omn = User.from_omniauth(request.env["omniauth.auth"])
+    session[:user_id] = @user_omn.id
+    redirect_to user_path(@user_omn)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   resources :users, except: [:index, :destroy, :new]
   put 'sign_in' => 'sessions#create', as: :sign_in
-  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:provider/callback', to: 'sessions#create_google'
   get 'auth/failure', to: redirect('/')
   delete 'log_out' => 'sessions#destroy', as: :log_out
 end
