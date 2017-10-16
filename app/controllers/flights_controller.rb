d85310eff526e363dce65498c18708af53d0272d
GeorgeSalari <x3m.giku@gmail.com>
2017-10-16 23:05:57 +0800

    add form for search flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,30 +5,30 @@ class FlightsController < ApplicationController
   end
 
   def google_api_respons
-    byebug
-    resp = send_request1
+    resp = send_request(params)
   end
 
-  def send_request
-    results = RestClient.post "https://www.googleapis.com/qpxExpress/v1/trips/search?key=#{ENV["GOOGLE_API_KEY"]}", {
+  def send_request(params)
+    byebug
+    results = RestClient.post "https://www.googleapis.com/qpxExpress/v1/trips/search?key=#{ENV[""]}", {
       "request": {
         "passengers": {
           "kind": "qpxexpress#passengerCounts",
-          "adultCount": 1,
-          "childCount": 1,
-          "infantInLapCount": 1,
-          "infantInSeatCount": 1,
-          "seniorCount": 1
+          "adultCount": params[:adultCount].to_i,
+          "childCount": params[:childCount].to_i,
+          "infantInLapCount": params[:infantInLapCount].to_i,
+          "infantInSeatCount": params[:infantInSeatCount].to_i,
+          "seniorCount": params[:seniorCount].to_i
         },
         "slice": [
           {
             "kind": "qpxexpress#sliceInput",
-            "origin": "KUL",
-            "destination": "HKT",
-            "date": "#{Date.today}",
-            "maxStops": 1,
-            "maxConnectionDuration": 3,
-            "preferredCabin": "",
+            "origin": params[:origin],
+            "destination": params[:destination],
+            "date": params[:date].to_date,
+            "maxStops": params[:maxStops].to_i || 10,
+            "maxConnectionDuration": params[:maxConnectionDuration].to_i || 24,
+            "preferredCabin": params[:preserredCabin] || "",
             "permittedDepartureTime": {
               "kind": "qpxexpress#timeOfDayRange",
               "earliestTime": "",
@@ -46,8 +46,8 @@ class FlightsController < ApplicationController
         "maxPrice": "",
         "saleCountry": "",
         "ticketingCountry": "",
-        "refundable": true,
-        "solutions": 20
+        "refundable": params[:refundable].to_i == 1 ,
+        "solutions": params[:solutions].to_i
       }
     }.to_json, {content_type: :json, accept: :json}
   end
